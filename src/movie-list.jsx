fc183a2019a90a285bbc85cd1c2bece893416673
paulryan17 <paulryanpersonal@gmail.com>
2019-07-21 17:07:30 +0100

    TIDY

--- src/movie-list.jsx
+++ src/movie-list.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { fetchMovies } from './fetch-movies'
+import React, { useState, useEffect } from 'react';
+import { fetchMovies } from './fetch-movies';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,54 +8,48 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { FixedSizeList as List } from 'react-window';
 
-const Movie = ({ Title, Production_Budget, Release_Date, Distributor, Director }) => {
+const Movie = ({
+  Title,
+  Production_Budget,
+  Release_Date,
+  Distributor,
+  Director,
+}) => {
   return (
     <Card>
-    <CardContent>
-      <Typography color="textSecondary" gutterBottom>
-        {Title}
-      </Typography>
-      <Typography variant="h5" component="h2">
-
-      </Typography>
-      <Typography color="textSecondary">
-        {Production_Budget}
-      </Typography>
-      <Typography variant="body2" component="p">
-        {Release_Date} {Distributor} {Director}
-      </Typography>
-    </CardContent>
-    <CardActions>
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-    > 
-      <Button variant="contained" size="small" color="primary">Learn More</Button>
-    </Grid>
-    </CardActions>
-  </Card>
-  )
-}
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {Title}
+        </Typography>
+        <Typography variant="h5" component="h2" />
+        <Typography color="textSecondary">{Production_Budget}</Typography>
+        <Typography variant="body2" component="p">
+          {Release_Date} {Distributor} {Director}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Button variant="contained" size="small" color="primary">
+            Learn More
+          </Button>
+        </Grid>
+      </CardActions>
+    </Card>
+  );
+};
 
 export const MovieList = () => {
-  const [movies, setMovies] = useState([])
-  const [showMovies, setShowMovies] = useState(false)
+  const [movies, setMovies] = useState([]);
+  const [showMovies, setShowMovies] = useState(false);
   useEffect(() => {
-    fetchMovies().then((res) => {
-      setMovies(res)
-    })
-  })
-  
-  return (
-     movies.map((movie, index) => {
-      if(index > 100){
-        return null
-      }
-      return  (
-        <Movie key={index} {...movie} />
-      )
-    })
-  )
-}
+    fetchMovies().then(res => {
+      setMovies(res);
+    });
+  });
+
+  const moviesList = movies.slice(0, 5000);
+
+  return moviesList.map((movie, index) => {
+    return <Movie key={index} {...movie} />;
+  });
+};
